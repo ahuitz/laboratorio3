fb26815c459e8964039d5792936afbd755f6ef71
EST1659109 <EST1659109@192.168.56.1>
2015-06-30 11:23:37 -0600

    paquete de NETBEANS

---
+++ control/src/control/Control.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control;
+
+/**
+ *
+ * @author EST1659109
+ */
+public class Control {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
