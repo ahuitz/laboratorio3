b68b7efc7f7bd8ed837a6832d365f20e2de3cc6d
yesica <yesicargueta@gmail.com>
2015-07-06 22:38:58 -0600

    clase control principal

---
+++ control/src/ControGeneral/Control.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ControGeneral;
+
+/**
+ *
+ * @author Cliente
+ */
+public class Control {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        AsignarLedBoton ct1 = new AsignarLedBoton();
+        ct1.asignarledBoton1();
+        ct1.asignarledBoton2();
+        ct1.asignarledBoton3();
+        ct1.asignarledBoton4();
+        ct1.asignarledBoton5();
+        ct1.asignarledBoton6();
+        
+    }
+    
+}
