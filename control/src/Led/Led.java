8e8d7134f482f3170426cfac87b0cfe79e2e672a
yesica <yesicargueta@gmail.com>
2015-07-06 22:32:47 -0600

    Clases para el control de los 3 leds

---
+++ control/src/Led/Led.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Led;
+
+/**
+ *
+ * @author Cliente
+ */
+public class Led {
+
+    public Led() {
+    }
+    
+    public void lenciende(){
+        System.out.println("led1 encendido");
+    }
+    public void lapagar(){
+        System.out.println("led1 apagado");
+    }
+    
+}
---
+++ control/src/Led/Led2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Led;
+
+/**
+ *
+ * @author Cliente
+ */
+public class Led2 {
+
+    public Led2() {
+    }
+    
+    
+    public void lenciende1(){
+        System.out.println("led2 encendido");
+    }
+    public void lapagar1(){
+        System.out.println("led2 apagado");
+    }
+}
